479e925ca566b7690bdb08544fee699f50732b54
porkbon <44909517+porkbon@users.noreply.github.com>
2024-07-22 15:41:27 -0500

    Add 'code_runner' config

---
+++ lua/custom/plugins/code_runner.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'CRAG666/code_runner.nvim',
+    config = true,
+    opts = {
+      mode = 'float',
+      float = {
+        border = 'single',
+        blend = 0,
+        border_hl = 'None',
+        float_hl = 'None',
+      },
+    },
+  },
+}
